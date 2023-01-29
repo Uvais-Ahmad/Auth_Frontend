1b3a8c137abf8617f7daffca538efbd726d61157
Uvais Ahmad <uvss21@gmail.com>
2023-01-29 12:02:13 +0530

    Registeration state set

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,27 +1,43 @@
 import { Button, Form, FormControl, FormGroup, FormLabel } from "react-bootstrap";
+import {useState} from 'react'
 function Register () {
+
+    const [userInfo , setUserInfo] = useState({
+        name:"",phone:"",password:"",confirm_password:""
+    })
+
+    function handleChange(e){
+        console.log(`${e.target.name} : ${e.target.value}`)
+        setUserInfo({...userInfo , [e.target.name] : e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log("useriNFO ",userInfo)
+    }
+
     return (
         <>
-        <Form className="Form h-75 px-5 py-3 mx-auto">
+        <Form className="Form h-75 px-5 py-3 mx-auto" onSubmit={handleSubmit}>
             <h1 style={{textAlign:'center'}}>Register</h1>
             <FormGroup className="my-3">
                 <FormLabel >Name</FormLabel>
-                <FormControl required={true} placeholder="Your name"></FormControl>
+                <FormControl required={true} onChange={handleChange} name="name" value={userInfo.name}  placeholder="Your name"></FormControl>
             </FormGroup>
 
             <FormGroup className="my-3">
                 <FormLabel >Phone</FormLabel>
-                <FormControl required={true} placeholder="Your new phone number" ></FormControl>
+                <FormControl required={true} onChange={handleChange} name="phone" value={userInfo.phone}  placeholder="Your new phone number" ></FormControl>
             </FormGroup>
 
             <FormGroup className="my-3">
                 <FormLabel >Password</FormLabel>
-                <FormControl required={true} placeholder="Password"></FormControl>
+                <FormControl required={true} onChange={handleChange} name="password" value={userInfo.password}  placeholder="Password"></FormControl>
             </FormGroup>
 
             <FormGroup className="my-3">
                 <FormLabel >Confirm Password</FormLabel>
-                <FormControl required={true} placeholder="Confirm password" ></FormControl>
+                <FormControl required={true} onChange={handleChange} name="confirm_password" value={userInfo.confirm_password}  placeholder="Confirm password" ></FormControl>
             </FormGroup>
 
             <Button style={{backgroundColor:'#090938',fontWeight:'bolder'}} className="w-100 mb-2" type="submit" >Register</Button>
